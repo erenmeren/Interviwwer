2c8e970637868748d67c082d2987a527a38740d5
kunteper <kunteper@kt.com>
2020-07-20 19:09:08 +0100

    Updates

---
+++ precache-manifest.6e1b542c53e9e300440b09c9e672796f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd53ecf6a1c7fe281f2cabe7c399776",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5abbc2e315966b65c14",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1b6ac0d67962ff81d562",
+    "url": "/static/css/main.0f410431.chunk.css"
+  },
+  {
+    "revision": "d5abbc2e315966b65c14",
+    "url": "/static/js/2.0c7b591e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0c7b591e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6ac0d67962ff81d562",
+    "url": "/static/js/main.258dcedd.chunk.js"
+  },
+  {
+    "revision": "0af834acac6bb9739980",
+    "url": "/static/js/runtime-main.fce7cc94.js"
+  }
+]);
