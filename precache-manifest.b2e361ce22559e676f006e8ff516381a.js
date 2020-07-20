c2958487d7cf640ae97bdef27ee9e1f8d2348411
kunteper <kunteper@kt.com>
2020-07-20 20:06:19 +0100

    Updates

---
+++ precache-manifest.b2e361ce22559e676f006e8ff516381a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca38c1a348bf5ee1d91f14d73e49e5c1",
+    "url": "/Interviwwer/index.html"
+  },
+  {
+    "revision": "d5abbc2e315966b65c14",
+    "url": "/Interviwwer/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5df44cddf3ff6a76692a",
+    "url": "/Interviwwer/static/css/main.0f410431.chunk.css"
+  },
+  {
+    "revision": "d5abbc2e315966b65c14",
+    "url": "/Interviwwer/static/js/2.0c7b591e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Interviwwer/static/js/2.0c7b591e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df44cddf3ff6a76692a",
+    "url": "/Interviwwer/static/js/main.046ca54e.chunk.js"
+  },
+  {
+    "revision": "74165a4ffdbde25573cd",
+    "url": "/Interviwwer/static/js/runtime-main.f5813dc2.js"
+  }
+]);
